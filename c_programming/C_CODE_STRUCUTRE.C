df2d7fee10d07dc36105664aec5b01a8c84bcbec
Witzcode0 <brijesh.gondaliya07@gmail.com>
2025-11-27 11:36:57 +0530

    datatypes, constants, structur

---
+++ c_programming/C_CODE_STRUCUTRE.C
@@ -0,0 +1,15 @@
+// documentation section
+// add two values
+
+// linking section
+#include<stdio.h>
+
+// global declaration section
+int num1 = 10, num2 = 20;
+
+// main function
+void main() {
+    printf("%d", num1 + num2);
+}
+
+// sub function decalration section
---
+++ c_programming/tokens/1.1_variable.c
@@ -0,0 +1,13 @@
+// concept : constant
+
+#include<stdio.h>
+
+void main(){
+    //float pi = 3.14;
+    //pi = 20.4;
+    //printf("%.2f", pi);
+
+    const float pi = 3.14;
+    pi = 20.4;
+    printf("%.2f", pi);
+}
